79d5c03a962860e1119aa31a130b28b9af2b1e8d
jamie <jinjiedeng.jjd@gmail.com>
2022-04-11 15:59:31 +0800

    fix bugs

--- lab/apr_11/solution.cc
+++ lab/apr_11/solution.cc
@@ -9,20 +9,20 @@
 void BFS
 (
   std::vector<std::vector<uint64_t>> const& graph,
-  std::vector<bool>& is_visited,
+  std::vector<bool>& is_in_deque,
   uint64_t source
 )
 {
   std::deque<uint64_t> queue;
-  queue.push_back(source);
+  queue.push_back(source); is_in_deque[source] = true;
   while (!queue.empty())
   {
     auto from {queue.front()}; queue.pop_front(); // this line is the only difference from DFS
     for (auto to : graph[from])
     {
-      if (!is_visited[to])
+      if (!is_in_deque[to])
       {
-        is_visited[to] = true;
+        is_in_deque[to] = true;
         queue.push_back(to);
       }
     }
@@ -33,20 +33,20 @@ void BFS
 void DFS
 (
   std::vector<std::vector<uint64_t>> const& graph,
-  std::vector<bool>& is_visited,
+  std::vector<bool>& is_in_deque,
   uint64_t source
 )
 {
   std::deque<uint64_t> stack;
-  stack.push_back(source);
+  stack.push_back(source); is_in_deque[source] = true;
   while (!stack.empty())
   {
     auto from {stack.back()}; stack.pop_back(); // this line is the only difference from BFS
     for (auto to : graph[from])
     {
-      if (!is_visited[to])
+      if (!is_in_deque[to])
       {
-        is_visited[to] = true;
+        is_in_deque[to] = true;
         stack.push_back(to);
       }
     }
@@ -57,15 +57,15 @@ void DFS
 uint64_t CountConnectedComponetns (std::vector<std::vector<uint64_t>> const& graph)
 {
   uint64_t count {};
-  std::vector<bool> is_visited(graph.size());
+  std::vector<bool> is_in_deque(graph.size());
   for (uint64_t source {1}; source != graph.size(); ++source)
   {
-    if (!is_visited[source])
+    if (!is_in_deque[source])
     {
       ++count;
       /* either BFS or DFS works */
-      BFS(graph, is_visited, source);
-      // DFS(graph, is_visited, source);
+      BFS(graph, is_in_deque, source);
+      // DFS(graph, is_in_deque, source);
     }
   }
   return count;
--- lab/apr_11/solution.py
+++ lab/apr_11/solution.py
@@ -4,37 +4,39 @@
 #
 from collections import deque
 
-def BFS (graph, is_visited, source):
+def BFS (graph, is_in_deque, source):
     queue = deque()
     queue.append(source)
+    is_in_deque[source] = True
     while len(queue):
         from_ = queue[0]
         queue.popleft() # this line and the previous line are the only difference from DFS
         for to in graph[from_]:
-            if not is_visited[to]:
-                is_visited[to] = True
+            if not is_in_deque[to]:
+                is_in_deque[to] = True
                 queue.append(to)
 
-def DFS (graph, is_visited, source):
+def DFS (graph, is_in_deque, source):
     stack = deque()
     stack.append(source)
+    is_in_deque[source] = True
     while len(stack):
         from_ = stack[-1]
         stack.pop() # this line and the previous line are the only difference from BFS
         for to in graph[from_]:
-            if not is_visited[to]:
-                is_visited[to] = True
+            if not is_in_deque[to]:
+                is_in_deque[to] = True
                 stack.append(to)
 
 def CountConnectedComponetns (graph):
     count = 0
-    is_visited = [False for _ in range(n + 1)]
-    for source in range(1, len(is_visited)):
-        if not is_visited[source]:
+    is_in_deque = [False for _ in range(n + 1)]
+    for source in range(1, len(is_in_deque)):
+        if not is_in_deque[source]:
             count += 1
             # either BFS or DFS works
-            BFS(graph, is_visited, source)
-            # DFS(graph, is_visited, source)
+            BFS(graph, is_in_deque, source)
+            # DFS(graph, is_in_deque, source)
     return count
 
 if __name__ == "__main__":
